649567054ccfe4e66897b16b5579d39db916af03
ammarbhatkar <36763132+ammarbhatkar@users.noreply.github.com>
2024-02-06 19:11:41 +0530

    seconf comiit
    secon commit

--- lib/constants/color_file.dart
+++ lib/constants/color_file.dart
@@ -6,3 +6,7 @@ const Color appLargeTextColor = Color.fromRGBO(0, 0, 0, 1);
 const Color primaryBlueColor = Color.fromRGBO(74, 144, 226, 1);
 const Color textFieldColor = Color.fromRGBO(74, 144, 226, 0.5);
 const Color buttonTextWhiteColor = Color.fromRGBO(255, 255, 255, 1);
+
+
+/// checkinggithub??
+/// 
